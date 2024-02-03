4b1df4b03d955374ffa8fc1aeea55982a4d18d29
Amanuel <amanueleman@gmail.com>
2024-02-03 17:33:00 -0500

    User model created

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: [true, "Username is Required"],
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email address Required"],
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", UserSchema);
+
+module.exports = User;
