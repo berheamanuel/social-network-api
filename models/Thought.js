b2a42c8107fb4818bc71e2cae7ab554d78068c1b
Amanuel <amanueleman@gmail.com>
2024-02-03 21:17:36 -0500

    Created Reaction schema and imported to thoughts model

--- models/Thought.js
+++ models/Thought.js
@@ -1,41 +1,6 @@
 const { Schema, model, Types } = require("mongoose");
+const ReactionSchema = require("./Reaction");
 const dateFormat = require("../utils/dateFormat");
-
-const ReactionSchema = new Schema(
-  {
-    reactionId: {
-      // Mongoose's ObjectId data type
-      type: Schema.Types.ObjectId,
-      // Default value is set to a new ObjectId
-      default: () => new Types.ObjectId(),
-    },
-
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280,
-    },
-
-    username: {
-      type: String,
-      required: true,
-    },
-
-    createdAt: {
-      type: Date,
-      // Set default value to the current timestamp
-      default: Date.now,
-      // Use a getter method to format the timestamp on query
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
-    id: false,
-  }
-);
 
 const ThoughtSchema = new Schema(
   {
@@ -50,7 +15,7 @@ const ThoughtSchema = new Schema(
       type: Date,
       default: Date.now,
       // Use a getter method to format the timestamp on query
-      get: (timestamp) => dateFormat(timestamp),
+      get: (date) => dateFormat(date),
     },
 
     username: {
